83b71bf24fbf5dd99f1bd29789e7ceb16e5ac155
Natalia Pechatnova <pechatnova.n@yandex.ru>
2022-02-24 18:46:21 +0300

    first

---
+++ scripts.js
@@ -0,0 +1,41 @@
+const balls = document.querySelector(".balls");
+const boy = document.querySelector(".boy");
+const start = document.querySelector(".start");
+const reset = document.querySelector(".reset");
+
+
+balls.style.bottom = '100px';
+
+const addAnimation = () => {
+
+    const pauseAnimation = () => {
+        cancelAnimationFrame(animation);
+    }
+
+
+    /*let valPixels = (Number(balls.style.bottom.split('').slice(0, -2).join('')));
+    balls.style.bottom = Number(valPixels + 2) + 'px';*/
+    if(balls.style.bottom <= "650px") {
+        let valPixels = (Number(balls.style.bottom.split('').slice(0, -2).join('')));
+        balls.style.bottom = Number(valPixels + 2) + 'px';
+        animation();
+    }
+    //start.addEventListener('click', pauseAnimation )
+}
+
+
+let animation = () => {
+    requestAnimationFrame(addAnimation)
+    start.classList.remove('pause')
+}
+
+
+
+const resetAnamation = () => {
+    balls.style.bottom = '100px';
+    cancelAnimationFrame(animation);
+}
+
+
+start.addEventListener('click', animation )
+reset.addEventListener('click', resetAnamation )
