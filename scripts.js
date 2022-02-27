21420630790d494b1857ef6f670f47e4c3875c65
Natalia Pechatnova <pechatnova.n@yandex.ru>
2022-02-27 12:59:08 +0300

    micropaterns

--- scripts.js
+++ scripts.js
@@ -1,3 +1,25 @@
 const text = document.querySelector('p')
 const input = document.querySelector('input[type=text]')
 
+
+function debounce (f, t) {
+    return function(args) {
+
+        let previousCall = this.lastCall;
+        this.lastCall = Date.now();
+        if (previousCall && ((this.lastCall-previousCall) <= t)) {
+            clearTimeout(this.lastCallTimer);
+        }
+        this.lastCallTimer = setTimeout(() => f(args), t);
+    }
+}
+
+
+const changeText = () => {
+    text.textContent = input.value;
+}
+
+input.addEventListener('input', debounce(changeText, 300) )
+
+
+
